e2932988aeffb031d2c7dacd3dafc63da3346cbd
Patricia Mago <patriciamagodev@gmail.com>
2023-09-11 18:27:20 -0400

    Agregar .gitignore y primeros archivos del proyecto

---
+++ script.js
@@ -0,0 +1,4 @@
+const h1 = document.querySelector("h1");
+const input1 = document.querySelector("calculo1");
+const input2 = document.querySelector("calculo2");
+const btn = document.querySelector("btnCalcular");
